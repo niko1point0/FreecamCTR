286c6571d18e62ec6d526448947172380eb9b49b
niko <crap@crap.com>
2020-08-01 11:47:54 -0400

    controller input

--- FreecamCTR/Main.cpp
+++ FreecamCTR/Main.cpp
@@ -1,8 +1,5 @@
 
 #include <windows.h>
-#include <time.h>
-#include <stdio.h>
-
 #include "Scanner.h"
 
 // Can be negative
@@ -28,9 +25,6 @@ void initialize()
 
 	// 300 + height of bar (35)
 	MoveWindow(console, r.left, r.top, 400, 300, TRUE);
-
-	// Initialize random number generator
-	srand((unsigned int)time(NULL));
 
 	printf("\n");
 	printf("Step 1: Open any ps1 emulator\n");
@@ -83,69 +77,25 @@ void initialize()
 	baseAddress -= 0x8003C62C;
 }
 
-void MoveCamera()
-{
-	// Player 1
-	int playerIndex = 0;
-	
-	// posX, posY, posZ, rotX, rotY, rotZ
-	short variables[6];
-	
-	// grab the position and rotation
-	ReadMem(0x80096B20 + 0x168 + 0x110 * playerIndex, variables, sizeof(variables));
-	
-	int speed = 0x18;
-	
-	if (GetAsyncKeyState(VK_UP))	variables[3] += speed;
-	if (GetAsyncKeyState(VK_DOWN))	variables[3] -= speed;
-	if (GetAsyncKeyState(VK_LEFT))	variables[4] += speed;
-	if (GetAsyncKeyState(VK_RIGHT)) variables[4] -= speed;
-	
-	float angle = 2 * 3.14159f * (float)variables[4] / (float)0x1000;
-	
-	if (GetAsyncKeyState('A'))
-	{
-		variables[2] += speed * sin(angle);
-		variables[0] -= speed * cos(angle);
-	}
-	
-	if (GetAsyncKeyState('D'))
-	{
-		variables[2] -= speed * sin(angle);
-		variables[0] += speed * cos(angle);
-	}
-	
-	if (GetAsyncKeyState('W'))
-	{
-		variables[0] -= speed * sin(angle);
-		variables[2] -= speed * cos(angle);
-	}
-	if (GetAsyncKeyState('S'))
-	{
-		variables[0] += speed * sin(angle);
-		variables[2] += speed * cos(angle);
-	}
-	
-	if (GetAsyncKeyState('F')) variables[1] -= speed;
-	if (GetAsyncKeyState('R')) variables[1] += speed;
-	
-	// inject the new position and rotation
-	WriteMem(0x80096B20 + 0x168 + 0x110 * playerIndex, variables, sizeof(variables));
-
-	int AddrP1;
-	ReadMem(0x8009900C, &AddrP1, sizeof(AddrP1));
-
-	int pos[3];
-	pos[0] = (int)variables[0] << 8;
-	pos[1] = (int)variables[1] << 8;
-	pos[2] = (int)variables[2] << 8;
-
-	int zero = 0;
-	WriteMem(AddrP1 + 0x2d4, pos, sizeof(pos));
-
-	// Disable the teleportation of the player to 2d4, 2d8, 2dc
-	WriteMem(AddrP1 + 0x7c, &zero, sizeof(zero));
-}
+#define UP 0x1
+#define DOWN 0x2
+#define LEFT 0x4
+#define RIGHT 0x8
+#define CROSS 0x10
+#define SQUARE 0x20
+#define CIRCLE 0x40
+//#define L2 0x80
+//#define L2 0x100 // which is it?
+#define R2 0x200
+#define R1 0x400
+#define L1 0x800
+#define START 0x1000
+#define SELECT 0x2000
+//#define CROSS 0x4000 -- are my notes right?
+//#define SQUARE 0x8000
+#define L3 0x10000
+#define R3 0x20000
+#define TRIANGLE 0x40000
 
 int main(int argc, char** argv)
 {
@@ -156,10 +106,85 @@ int main(int argc, char** argv)
 	// Main loop...
 	while (true)
 	{
+		// Allow us to move the camera
 		int status = 0;
 		WriteMem(0x80098000, &status, sizeof(status));
 
-		MoveCamera();
+		// posX, posY, posZ, rotX, rotY, rotZ
+		short variables[6];
+
+		// If you want to mess with other cameras, each additional
+		// camera is 0x110 bytes added to the address
+
+		// grab the position and rotation
+		ReadMem(0x80096B20 + 0x168, variables, sizeof(variables));
+
+		int speed = 0x18;
+
+		// Get Controller Data
+		// If you want input from other controllers, each additional
+		// controlller is 0x50 bytes added to the address. Also, 
+		// you can use + 0x14 instead of 0x10 for 'tap' instead of 'hold'
+		int buttons;
+		ReadMem(0x80096804 + 0x10, &buttons, sizeof(buttons));
+
+		if (buttons & SELECT) speed *= 3;
+
+		if (buttons & TRIANGLE) variables[3] += speed;
+		if (buttons & CROSS) variables[3] -= speed;
+		if (buttons & SQUARE) variables[4] += speed;
+		if (buttons & CIRCLE) variables[4] -= speed;
+
+		float angle = 2 * 3.14159f * (float)variables[4] / (float)0x1000;
+
+		if (buttons & LEFT)
+		{
+			variables[0] -= speed * cos(angle);
+			variables[2] += speed * sin(angle);
+		}
+
+		else if (buttons & RIGHT)
+		{
+			variables[0] += speed * cos(angle);
+			variables[2] -= speed * sin(angle);
+		}
+
+		if (buttons & UP)
+		{
+			variables[0] -= speed * sin(angle);
+			variables[2] -= speed * cos(angle);
+		}
+		
+		else if (buttons & DOWN)
+		{
+			variables[0] += speed * sin(angle);
+			variables[2] += speed * cos(angle);
+		}
+
+		if (buttons & L1) variables[1] -= speed;
+		if (buttons & R1) variables[1] += speed;
+
+		// inject the new position and rotation
+		WriteMem(0x80096B20 + 0x168, variables, sizeof(variables));
+
+		// Get address of P1's 0x670 buffer
+		int AddrP1;
+		ReadMem(0x8009900C, &AddrP1, sizeof(AddrP1));
+
+		// Compress position to shorts
+		int pos[3];
+		pos[0] = (int)variables[0] << 8;
+		pos[1] = (int)variables[1] << 8;
+		pos[2] = (int)variables[2] << 8;
+
+		// Change player position to camera position
+		int zero = 0;
+		WriteMem(AddrP1 + 0x2d4, pos, sizeof(pos));
+
+		// +7c
+		// Disable the teleportation of the player to 2d4, 2d8, 2dc,
+		// so player wont use it's own position variables
+		WriteMem(AddrP1 + 0x7c, &zero, sizeof(zero));
 
 		// 16ms = 60fps
 		Sleep(16);
